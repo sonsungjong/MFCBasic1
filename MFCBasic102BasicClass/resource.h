167022fa957932bd2fc9ab9dd7e3e0ea94752828
sonsungjong <67694475+sonsungjong@users.noreply.github.com>
2021-06-29 10:05:09 +0900

    버튼 추가했지롱

--- MFCBasic102BasicClass/resource.h
+++ MFCBasic102BasicClass/resource.h
@@ -4,6 +4,7 @@
 //
 #define IDD_MFCBASIC102BASICCLASS_DIALOG 102
 #define IDR_MAINFRAME                   128
+#define IDC_BUTTON1                     1000
 
 // Next default values for new objects
 // 
@@ -11,7 +12,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        130
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
