769f815f7e33e9e5e215aff4255d8ac79f94213c
tjdwhd70599@gmail.com <tjdwhd70599@gmail.com>
2021-11-08 13:43:02 +0900

    원으로 화살표만들기

--- MFCBasic219Circle3/MFCBasic219Circle3Dlg.cpp
+++ MFCBasic219Circle3/MFCBasic219Circle3Dlg.cpp
@@ -7,6 +7,7 @@
 #include "MFCBasic219Circle3.h"
 #include "MFCBasic219Circle3Dlg.h"
 #include "afxdialogex.h"
+#include <cmath>
 
 #ifdef _DEBUG
 #define new DEBUG_NEW
@@ -31,6 +32,10 @@ void CMFCBasic219Circle3Dlg::DoDataExchange(CDataExchange* pDX)
 BEGIN_MESSAGE_MAP(CMFCBasic219Circle3Dlg, CDialogEx)
 	ON_WM_PAINT()
 	ON_WM_QUERYDRAGICON()
+	ON_WM_DESTROY()
+	ON_WM_LBUTTONDOWN()
+	ON_WM_LBUTTONUP()
+	ON_WM_MOUSEMOVE()
 END_MESSAGE_MAP()
 
 
@@ -46,6 +51,13 @@ BOOL CMFCBasic219Circle3Dlg::OnInitDialog()
 	SetIcon(m_hIcon, FALSE);		// Set small icon
 
 	// TODO: Add extra initialization here
+	GetClientRect(m_rect);
+	m_image.Create(m_rect.Width(), m_rect.Height(), 32);
+	m_image_dc.Attach(m_image.GetDC());
+
+	m_green_pen.CreatePen(PS_SOLID, 1, RGB(100, 255, 100));
+	m_green_brush.CreateSolidBrush(RGB(0, 160, 0));
+	SetBackgroundColor(RGB(0, 0, 0));
 
 	return TRUE;  // return TRUE  unless you set the focus to a control
 }
@@ -56,9 +68,9 @@ BOOL CMFCBasic219Circle3Dlg::OnInitDialog()
 
 void CMFCBasic219Circle3Dlg::OnPaint()
 {
+		CPaintDC dc(this); // device context for painting
 	if (IsIconic())
 	{
-		CPaintDC dc(this); // device context for painting
 
 		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
 
@@ -75,7 +87,8 @@ void CMFCBasic219Circle3Dlg::OnPaint()
 	}
 	else
 	{
-		CDialogEx::OnPaint();
+		//CDialogEx::OnPaint();
+		m_image.Draw(dc, 0, 0);
 	}
 }
 
@@ -86,3 +99,71 @@ HCURSOR CMFCBasic219Circle3Dlg::OnQueryDragIcon()
 	return static_cast<HCURSOR>(m_hIcon);
 }
 
+
+
+void CMFCBasic219Circle3Dlg::OnDestroy()
+{
+	CDialogEx::OnDestroy();
+
+	// TODO: Add your message handler code here
+	m_image_dc.Detach();
+	m_image.ReleaseDC();
+
+	m_green_pen.DeleteObject();
+	m_green_brush.DeleteObject();
+}
+
+
+void CMFCBasic219Circle3Dlg::OnLButtonDown(UINT nFlags, CPoint point)
+{
+	// TODO: Add your message handler code here and/or call default
+	m_is_clicked = 1;
+	m_start_pos = point;
+
+	CDialogEx::OnLButtonDown(nFlags, point);
+}
+
+
+void CMFCBasic219Circle3Dlg::OnLButtonUp(UINT nFlags, CPoint point)
+{
+	// TODO: Add your message handler code here and/or call default
+	m_is_clicked = 0;
+
+	CDialogEx::OnLButtonUp(nFlags, point);
+}
+
+
+void CMFCBasic219Circle3Dlg::OnMouseMove(UINT nFlags, CPoint point)
+{
+	// TODO: Add your message handler code here and/or call default
+	if (m_is_clicked == 1) {
+		// 이전꺼 있으면 지우기
+		m_image_dc.FillSolidRect(m_rect, RGB(0, 0, 0));
+
+		m_image_dc.SelectObject(&m_green_pen);
+		//m_image_dc.SelectStockObject(NULL_BRUSH);
+		//m_image_dc.Ellipse(point.x-50, point.y-50, point.x+50, point.y+50);
+
+		m_image_dc.MoveTo(m_start_pos);
+		m_image_dc.LineTo(point);
+
+		POINT pos_list[3];
+		pos_list[0].x = point.x;
+		pos_list[0].y = point.y;
+
+		double radian = atan2(m_start_pos.y - point.y, point.x - m_start_pos.x);
+
+		pos_list[1].x = (int)(cos(radian+5.0/6.0*PI)*50) + point.x;
+		pos_list[1].y = (int)(sin(radian + 5.0 / 6.0 * PI) * -50) + point.y;
+
+		pos_list[2].x = (int)(cos(radian + 7.0 / 6.0 * PI) * 50) + point.x;
+		pos_list[2].y = (int)(sin(radian + 7.0 / 6.0 * PI) * -50) + point.y;
+
+		m_image_dc.SelectObject(&m_green_brush);
+		m_image_dc.Polygon(pos_list, 3);
+
+		::InvalidateRect(m_hWnd, NULL, FALSE);
+	}
+
+	CDialogEx::OnMouseMove(nFlags, point);
+}
--- MFCBasic219Circle3/MFCBasic219Circle3Dlg.h
+++ MFCBasic219Circle3/MFCBasic219Circle3Dlg.h
@@ -3,11 +3,24 @@
 //
 
 #pragma once
+#define PI 3.141592
 
 
 // CMFCBasic219Circle3Dlg dialog
 class CMFCBasic219Circle3Dlg : public CDialogEx
 {
+protected:
+	CImage m_image;
+	CDC m_image_dc;
+
+	CPen m_green_pen;
+	CBrush m_green_brush;
+
+	CRect m_rect;
+	
+	char m_is_clicked = 0;
+	CPoint m_start_pos;
+
 // Construction
 public:
 	CMFCBasic219Circle3Dlg(CWnd* pParent = nullptr);	// standard constructor
@@ -30,4 +43,9 @@ protected:
 	afx_msg void OnPaint();
 	afx_msg HCURSOR OnQueryDragIcon();
 	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnDestroy();
+	afx_msg void OnLButtonDown(UINT nFlags, CPoint point);
+	afx_msg void OnLButtonUp(UINT nFlags, CPoint point);
+	afx_msg void OnMouseMove(UINT nFlags, CPoint point);
 };
