087527417e7241e2681640c890401db2312703d6
sonsungjong <tjdwhd70599@gmail.com>
2022-12-12 02:01:41 +0900

    DCP 클래스

--- MFCL225DCPclass/DCP.cpp
+++ MFCL225DCPclass/DCP.cpp
@@ -1,8 +1,8 @@
 #include "pch.h"
 #include "DCP.h"
 
-// static멤버변수 : 클래스 멤버만 공유하는 전역변수
-int DCP::m_object_count = 0;
+// static멤버변수(클래스 멤버만 공유할 수 있는 전역변수) 초기화
+INT DCP::m_object_count = 0;
 ULONG_PTR DCP::m_token = 0;
 
 DCP::DCP()
@@ -10,7 +10,7 @@ DCP::DCP()
 	Init();
 }
 
-DCP::DCP(int a_width, int a_height)
+DCP::DCP(INT a_width, INT a_height)
 {
 	Init();
 	CreateDCP(a_width, a_height);
@@ -30,34 +30,43 @@ DCP::~DCP()
 		delete mp_bitmap;
 	}
 
-	m_object_count--;
-	if (m_object_count == 0) {
-		GdiplusShutdown(m_token);
+	m_object_count--;						// DCP 객체수 감소
+	if (m_object_count == 0) {							// 마지막 객체가 해제될 때 GDI+ 를 해제
+		Gdiplus::GdiplusShutdown(m_token);
 	}
 }
 
 void DCP::Init()
 {
-	if (m_object_count == 0) {
-		GdiplusStartupInput gpsi;
-		GdiplusStartup(&m_token, &gpsi, NULL);
+	if (m_object_count == 0) {							// 첫 DCP 객체라면 GDI+ 를 초기화 시킨다.
+		Gdiplus::GdiplusStartupInput gpsi;
+		Gdiplus::GdiplusStartup(&m_token, &gpsi, NULL);
 	}
-	m_object_count++;
-	m_last_position_x = m_last_position_y = m_width = m_height = 0;
-	mp_bitmap = NULL;
-	mp_graphic = NULL;
+	m_object_count++;						// DCP 객체수 증가
+	// 좌표 및 크기 정보 초기화
+	m_last_position_x = 0;
+	m_last_position_y = 0;
+	m_width = 0;
+	m_height = 0;
 
-	mp_brush = new SolidBrush(RGB24(255, 255, 255));
-	mp_font_brush = new SolidBrush(RGB24(0, 0, 0));
-	mp_pen = new Pen(RGB24(0, 0, 0));
-	mp_font = new Gdiplus::Font(_T("굴림체"), 9);
+	mp_bitmap = NULL;						// 출력 이미지를 저장할 이미지 객체
+	mp_graphic = NULL;						// 그림 그리기 객체
+
+	mp_brush = new Gdiplus::SolidBrush(RGB24(255, 255, 255));				// 기본 브러시
+	mp_font_brush = new Gdiplus::SolidBrush(RGB24(0, 0, 0));					// 기본 브러시 (글꼴용)
+	mp_pen = new Gdiplus::Pen(RGB24(0, 0, 0));				// 선 색상
+	mp_font = new Gdiplus::Font(_T("굴림체"), 9);				// 기본 글꼴
 }
 
-void DCP::CreateDCP(int a_width, int a_height)
+void DCP::CreateDCP(INT a_width, INT a_height)
 {
+	// 출력 장치가 생성되어 있지 않거나 폭과 높이가 달라야만 생성한다.
 	if (mp_graphic == NULL || m_width != a_width || m_height != a_height) {
+		// 새로운 폭과 높이 설정
 		m_width = a_width;
 		m_height = a_height;
+
+		// 기존 객체 제거
 		if (mp_graphic != NULL) {
 			delete mp_graphic;
 		}
@@ -65,44 +74,66 @@ void DCP::CreateDCP(int a_width, int a_height)
 			delete mp_bitmap;
 		}
 
-		mp_bitmap = new Bitmap(m_width, m_height);
+		// 새로운 크기에 맞게 비트맵과 출력 그래픽 객체 생성
+		mp_bitmap = new Gdiplus::Bitmap(m_width, m_height);
 		mp_graphic = new Gdiplus::Graphics(mp_bitmap);
 
-		mp_graphic->SetSmoothingMode(SmoothingModeAntiAlias);
+		// 'AntiAlias' 사용
+		mp_graphic->SetSmoothingMode(Gdiplus::SmoothingModeAntiAlias);
 	}
 }
 
-void DCP::Rectangle(int a_sx, int a_sy, int a_ex, int a_ey)
+// a_color로 전체 영역을 채운다.
+void DCP::Clear(Gdiplus::ARGB a_color)
 {
+	mp_graphic->Clear(a_color);
+}
+
+// 설정된 Brush 색상으로 전체 영역을 채운다.
+void DCP::Clear()
+{
+	mp_graphic->Clear(m_temp_color);
+}
+
+// 사각형을 그린다 (기존 브러시와 펜을 사용)
+void DCP::Rectangle(INT a_sx, INT a_sy, INT a_ex, INT a_ey)
+{
+	// 내부 채우기
 	mp_graphic->FillRectangle(mp_brush, a_sx, a_sy, a_ex - a_sx, a_ey - a_sy);
+	// 테두리 그리기
 	mp_graphic->DrawRectangle(mp_pen, a_sx, a_sy, a_ex - a_sx - 1, a_ey - a_sy - 1);
 }
 
-void DCP::FillSolidRect(int a_sx, int a_sy, int a_ex, int a_ey, ARGB a_color)
+// 지정한 색상으로 사각형을 그린다 (채우기)
+void DCP::FillSolidRect(INT a_sx, INT a_sy, INT a_ex, INT a_ey, Gdiplus::ARGB a_color)
 {
 	m_temp_color.SetValue(a_color);
-	((SolidBrush*)mp_brush)->SetColor(m_temp_color);
+	((Gdiplus::SolidBrush*)mp_brush)->SetColor(m_temp_color);
 	mp_graphic->FillRectangle(mp_brush, a_sx, a_sy, a_ex - a_sx, a_ey - a_sy);
 }
 
-void DCP::FillSolidRect(int a_sx, int a_sy, int a_ex, int a_ey)
+// 사각형을 그린다 (채우기)
+void DCP::FillSolidRect(INT a_sx, INT a_sy, INT a_ex, INT a_ey)
 {
 	mp_graphic->FillRectangle(mp_brush, a_sx, a_sy, a_ex - a_sx, a_ey - a_sy);
 }
 
-void DCP::DrawRect(int a_sx, int a_sy, int a_ex, int a_ey, ARGB a_color)
+// 지정한 색상으로 사각형을 그린다 (테두리)
+void DCP::DrawRect(INT a_sx, INT a_sy, INT a_ex, INT a_ey, Gdiplus::ARGB a_color)
 {
 	m_temp_color.SetValue(a_color);
 	mp_pen->SetColor(m_temp_color);
 	mp_graphic->DrawRectangle(mp_pen, a_sx, a_sy, a_ex - a_sx - 1, a_ey - a_sy - 1);
 }
 
-void DCP::DrawRect(int a_sx, int a_sy, int a_ex, int a_ey)
+// 사각형을 그린다 (테두리)
+void DCP::DrawRect(INT a_sx, INT a_sy, INT a_ex, INT a_ey)
 {
 	mp_graphic->DrawRectangle(mp_pen, a_sx, a_sy, a_ex - a_sx - 1, a_ey - a_sy - 1);
 }
 
-void DCP::Ellipse(int a_sx, int a_sy, int a_ex, int a_ey)
+// 타원을 그린다. (채우기와 테두리 모두 수행, 기존의 사용하던 브러시와 펜으로)
+void DCP::Ellipse(INT a_sx, INT a_sy, INT a_ex, INT a_ey)
 {
 	// 내부 채우기
 	mp_graphic->FillEllipse(mp_brush, a_sx, a_sy, a_ex - a_sx, a_ey - a_sy);
@@ -110,31 +141,36 @@ void DCP::Ellipse(int a_sx, int a_sy, int a_ex, int a_ey)
 	mp_graphic->DrawEllipse(mp_pen, a_sx, a_sy, a_ex - a_sx - 1, a_ey - a_sy - 1);
 }
 
-void DCP::FillSolidEllipse(int a_sx, int a_sy, int a_ex, int a_ey, ARGB a_color)
+// 지정한 색상으로 타원을 그린다 (채우기만 수행)
+void DCP::FillSolidEllipse(INT a_sx, INT a_sy, INT a_ex, INT a_ey, Gdiplus::ARGB a_color)
 {
 	m_temp_color.SetValue(a_color);
-	((SolidBrush*)mp_brush)->SetColor(m_temp_color);
+	((Gdiplus::SolidBrush*)mp_brush)->SetColor(m_temp_color);
 	mp_graphic->FillEllipse(mp_brush, a_sx, a_sy, a_ex - a_sx, a_ey - a_sy);
 }
 
-void DCP::FillSolidEllipse(int a_sx, int a_sy, int a_ex, int a_ey)
+// 기존 브러시로 타원을 그린다 (채우기만 수행)
+void DCP::FillSolidEllipse(INT a_sx, INT a_sy, INT a_ex, INT a_ey)
 {
 	mp_graphic->FillEllipse(mp_brush, a_sx, a_sy, a_ex - a_sx, a_ey - a_sy);
 }
 
-void DCP::DrawEllipse(int a_sx, int a_sy, int a_ex, int a_ey, ARGB a_color)
+// 지정한 색상으로 타원을 그린다 (테두리)
+void DCP::DrawEllipse(INT a_sx, INT a_sy, INT a_ex, INT a_ey, Gdiplus::ARGB a_color)
 {
 	m_temp_color.SetValue(a_color);
 	mp_pen->SetColor(m_temp_color);
 	mp_graphic->DrawEllipse(mp_pen, a_sx, a_sy, a_ex - a_sx - 1, a_ey - a_sy - 1);
 }
 
-void DCP::DrawEllipse(int a_sx, int a_sy, int a_ex, int a_ey)
+// 타원을 그린다 (테두리)
+void DCP::DrawEllipse(INT a_sx, INT a_sy, INT a_ex, INT a_ey)
 {
 	mp_graphic->DrawEllipse(mp_pen, a_sx, a_sy, a_ex - a_sx - 1, a_ey - a_sy - 1);
 }
 
-void DCP::DrawLine(int a_sx, int a_sy, int a_ex, int a_ey, ARGB a_color)
+// 펜으로 선을 그린다 (시작점과 끝점 사용, 색상지정 가능)
+void DCP::DrawLine(INT a_sx, INT a_sy, INT a_ex, INT a_ey, Gdiplus::ARGB a_color)
 {
 	m_temp_color.SetValue(a_color);
 	mp_pen->SetColor(m_temp_color);
@@ -143,39 +179,63 @@ void DCP::DrawLine(int a_sx, int a_sy, int a_ex, int a_ey, ARGB a_color)
 	m_last_position_y = a_ey;
 }
 
-void DCP::DrawLine(int a_sx, int a_sy, int a_ex, int a_ey)
+// 펜으로 선을 그린다. (시작점과 끝점 사용)
+void DCP::DrawLine(INT a_sx, INT a_sy, INT a_ex, INT a_ey)
 {
 	mp_graphic->DrawLine(mp_pen, a_sx, a_sy, a_ex, a_ey);
 	m_last_position_x = a_ex;
-	m_last_position_y = a_ey;
+	m_last_position_y = a_ex;
 }
 
-void DCP::MoveTo(int a_x, int a_y)
+// 선의 시작점을 정한다. 선은 그려지지 않는다.
+void DCP::MoveTo(INT a_x, INT a_y)
 {
 	m_last_position_x = a_x;
 	m_last_position_y = a_y;
 }
 
-void DCP::LineTo(int a_x, int a_y)
+// 이전에 그린 선 또는 MoveTo로 지정한 점으로부터 끝점과 연결하는 선을 그린다.
+void DCP::LineTo(INT a_x, INT a_y)
 {
 	mp_graphic->DrawLine(mp_pen, m_last_position_x, m_last_position_y, a_x, a_y);
 	m_last_position_x = a_x;
 	m_last_position_y = a_y;
 }
 
-void DCP::SetDCBrushColor(ARGB a_color)
+// 속이 채워진 다각형 그리기
+void DCP::FillPolygon(Gdiplus::Point* ap_position_list, INT a_count)
 {
-	m_temp_color.SetValue(a_color);
-	((SolidBrush*)mp_brush)->SetColor(m_temp_color);
+	mp_graphic->FillPolygon(mp_brush, ap_position_list, a_count);
 }
 
-void DCP::SetDCPenColor(ARGB a_color)
+// 다각형 테두리 그리기
+void DCP::DrawPolygon(Gdiplus::Point* ap_position_list, INT a_count)
+{
+	mp_graphic->DrawPolygon(mp_pen, ap_position_list, a_count);
+}
+
+// Arc 테두리 그리기
+void DCP::DrawArc(Gdiplus::REAL a_x, Gdiplus::REAL a_y, Gdiplus::REAL a_cx, Gdiplus::REAL a_cy, Gdiplus::REAL a_start_angle, Gdiplus::REAL a_sweep_angle)
+{
+	mp_graphic->DrawArc(mp_pen, a_x, a_y, a_cx, a_cy, a_start_angle, a_sweep_angle);
+}
+
+// 브러시 색상 변경
+void DCP::SetDCBrushColor(Gdiplus::ARGB a_color)
+{
+	m_temp_color.SetValue(a_color);
+	((Gdiplus::SolidBrush*)mp_brush)->SetColor(m_temp_color);
+}
+
+// 팬 색상 변경
+void DCP::SetDCPenColor(Gdiplus::ARGB a_color)
 {
 	m_temp_color.SetValue(a_color);
 	mp_pen->SetColor(m_temp_color);
 }
 
-void DCP::SetDCPenColor(DashStyle a_style, REAL a_width, ARGB a_color)
+// 팬 변경 (스타일, 굵기, 색상)
+void DCP::SetDCPenColor(Gdiplus::DashStyle a_style, Gdiplus::REAL a_width, Gdiplus::ARGB a_color)
 {
 	m_temp_color.SetValue(a_color);
 	mp_pen->SetColor(m_temp_color);
@@ -183,41 +243,76 @@ void DCP::SetDCPenColor(DashStyle a_style, REAL a_width, ARGB a_color)
 	mp_pen->SetDashStyle(a_style);
 }
 
-void DCP::SetTextColor(ARGB a_color)
+// 글꼴 색상 변경
+void DCP::SetTextColor(Gdiplus::ARGB a_color)
 {
 	m_temp_color.SetValue(a_color);
-	((SolidBrush*)mp_font_brush)->SetColor(m_temp_color);
+	((Gdiplus::SolidBrush*)mp_font_brush)->SetColor(m_temp_color);
 }
 
+// 폰트 객체 재설정
 Gdiplus::Font* DCP::SelectObject(Gdiplus::Font* ap_font)
 {
 	Gdiplus::Font* p_old_font = mp_font;
 	mp_font = ap_font;
-	return p_old_font;
+	return p_old_font;					// 이전 폰트 객체의 주소를 반환
 }
 
-void DCP::TextOut(int a_x, int a_y, ARGB a_color, const TCHAR* ap_str, int a_str_len)
+// 문자열 출력 (색상)
+void DCP::GpTextOut(INT a_x, INT a_y, Gdiplus::ARGB a_color, const TCHAR* ap_str, INT a_str_len)
 {
 	m_temp_color.SetValue(a_color);
-	((SolidBrush*)mp_font_brush)->SetColor(m_temp_color);
-	mp_graphic->DrawString(ap_str, a_str_len, mp_font, PointF(REAL(a_x), REAL(a_y)), mp_font_brush);
+	((Gdiplus::SolidBrush*)mp_font_brush)->SetColor(m_temp_color);
+	mp_graphic->DrawString(ap_str, a_str_len, mp_font, Gdiplus::PointF(Gdiplus::REAL(a_x), Gdiplus::REAL(a_y)), mp_font_brush);
 }
 
-void DCP::TextOut(int a_x, int a_y, const TCHAR* ap_str, int a_str_len)
+// 문자열 출력
+void DCP::GpTextOut(INT a_x, INT a_y, const TCHAR* ap_str, INT a_str_len)
 {
-	mp_graphic->DrawString(ap_str, a_str_len, mp_font, PointF(REAL(a_x), REAL(a_y)), mp_font_brush);
+	mp_graphic->DrawString(ap_str, a_str_len, mp_font, Gdiplus::PointF(Gdiplus::REAL(a_x), Gdiplus::REAL(a_y)), mp_font_brush);
 }
 
-void DCP::Draw(Image* ap_image, int a_x, int a_y)
+// 이미지 객체를 출력
+void DCP::Draw(Gdiplus::Image* ap_image, INT a_x, INT a_y)
 {
 	// 이미지 출력
 	mp_graphic->DrawImage(ap_image, a_x, a_y);
 }
 
-void DCP::Draw(HDC ah_dc, int a_x, int a_y)
+// 경로를 받아 해당 이미지 출력
+void DCP::Draw(TCHAR* a_file_path, INT a_x, INT a_y)
+{
+	Gdiplus::Image* image = Gdiplus::Image::FromFile(a_file_path);
+	// 이미지 출력
+	mp_graphic->DrawImage(image, a_x, a_y);
+}
+
+// DC에 현재 이미지를 출력
+void DCP::Draw(HDC ah_dc, INT a_x, INT a_y)
 {
 	// 전달된 DC에 현재 이미지를 출력
 	Gdiplus::Graphics* p_graphic = new Gdiplus::Graphics(ah_dc);
-	p_graphic->DrawImage(mp_bitmap, 0, 0);
+	p_graphic->DrawImage(mp_bitmap, a_x, a_y);
 	delete p_graphic;
 }
+
+Gdiplus::Image* DCP::GetImage()
+{
+	return mp_bitmap;
+}
+
+Gdiplus::Graphics* DCP::GetGraphics()
+{
+	return mp_graphic;
+}
+
+DCP::operator Gdiplus::Image* ()
+{
+	return mp_bitmap;
+}
+
+DCP::operator Gdiplus::Graphics* ()
+{
+	return mp_graphic;
+}
+
--- MFCL225DCPclass/DCP.h
+++ MFCL225DCPclass/DCP.h
@@ -1,73 +1,112 @@
 #ifndef DCP_H_
 #define DCP_H_
 
+// 클래스 초기 모델 제작 : 김성엽(tipsware@naver.com)
+// 클래스 배포 : https://cafe.naver.com/mythread
+// 처음 만든 날짜 : 2020년 3월 24일 (화요일)
+// 처음 마지막 업데이트 날짜 : 2020년 3월 25일 (수요일)
+
+// 이 클래스의 기능이나 내용을 변경했다면 아래에 이력을 남겨주시면 됩니다.
+// 클래스 내용 변경자 : 손성종
+// 마지막 변경 날짜 : 2022년 12월 9일 (금요일)
+
 #include <gdiplus.h>
-using namespace Gdiplus;
 
-#define RGB24(r,g,b) (0xFF000000 | (r << 16) | (g << 8) | b)
-#define RGB32(a,r,g,b) ( (a<<24)| (r << 16) | (g << 8) | b)
+#define RGB24(r,g,b)(0xFF000000 | (r << 16) | (g << 8) | b)
+#define RGB32(a,r,g,b)((a<<24)|(r<<16)|(g<<8)|b)
 
+// DC Plus 클래스, 더블버퍼링 사용
 class DCP
 {
-protected:
-	Brush* mp_brush;
-	Brush* mp_font_brush;
-	Pen* mp_pen;
+private:
+	Gdiplus::Brush* mp_brush;
+	Gdiplus::Brush* mp_font_brush;
+	Gdiplus::Pen* mp_pen;
 	Gdiplus::Font* mp_font;
-	Bitmap* mp_bitmap;
-	Graphics* mp_graphic;
+	Gdiplus::Bitmap* mp_bitmap;
+	Gdiplus::Graphics* mp_graphic;
 
-	int m_width, m_height;
-	int m_last_position_x, m_last_position_y;
-	Color m_temp_color;
+	INT m_width;				// 출력 폭
+	INT m_height;			// 출력 높이
+	INT m_last_position_x;
+	INT m_last_position_y;
+	Gdiplus::Color m_temp_color;					// 색상 변경을 위한 임시 객체
 
-	static ULONG_PTR m_token;
-	static int m_object_count;
+	static ULONG_PTR m_token;				// GDI+ 라이브러리 사용 정보
+	static INT m_object_count;					// DCP 객체 갯수 계산
+
 public:
-	DCP();
-	DCP(int a_width, int a_height);
+	explicit DCP();
+	explicit DCP(INT a_width, INT a_height);
 	virtual ~DCP();
 
 	void Init();
-	void CreateDCP(int a_width, int a_height);
+	void CreateDCP(INT a_width, INT a_height);
 
 	// 사각형
-	void Rectangle(int a_sx, int a_sy, int a_ex, int a_ey);
-	void FillSolidRect(int a_sx, int a_sy, int a_ex, int a_ey, ARGB a_color);
-	void FillSolidRect(int a_sx, int a_sy, int a_ex, int a_ey);
-	void DrawRect(int a_sx, int a_sy, int a_ex, int a_ey, ARGB a_color);
-	void DrawRect(int a_sx, int a_sy, int a_ex, int a_ey);
+	void Rectangle(INT a_sx, INT a_sy, INT a_ex, INT a_ey);
+	void FillSolidRect(INT a_sx, INT a_sy, INT a_ex, INT a_ey, Gdiplus::ARGB a_color);
+	void FillSolidRect(INT a_sx, INT a_sy, INT a_ex, INT a_ey);
+	void DrawRect(INT a_sx, INT a_sy, INT a_ex, INT a_ey, Gdiplus::ARGB a_color);
+	void DrawRect(INT a_sx, INT a_sy, INT a_ex, INT a_ey);
 
 	// 타원
-	void Ellipse(int a_sx, int a_sy, int a_ex, int a_ey);
-	void FillSolidEllipse(int a_sx, int a_sy, int a_ex, int a_ey, ARGB a_color);
-	void FillSolidEllipse(int a_sx, int a_sy, int a_ex, int a_ey);
-	void DrawEllipse(int a_sx, int a_sy, int a_ex, int a_ey, ARGB a_color);
-	void DrawEllipse(int a_sx, int a_sy, int a_ex, int a_ey);
+	void Ellipse(INT a_sx, INT a_sy, INT a_ex, INT a_ey);
+	void FillSolidEllipse(INT a_sx, INT a_sy, INT a_ex, INT a_ey, Gdiplus::ARGB a_color);
+	void FillSolidEllipse(INT a_sx, INT a_sy, INT a_ex, INT a_ey);
+	void DrawEllipse(INT a_sx, INT a_sy, INT a_ex, INT a_ey, Gdiplus::ARGB a_color);
+	void DrawEllipse(INT a_sx, INT a_sy, INT a_ex, INT a_ey);
 
-	// 선 그리기
-	void DrawLine(int a_sx, int a_sy, int a_ex, int a_ey, ARGB a_color);
-	void DrawLine(int a_sx, int a_sy, int a_ex, int a_ey);
-	void MoveTo(int a_x, int a_y);
-	void LineTo(int a_x, int a_y);
+	// 선그리기
+	void DrawLine(INT a_sx, INT a_sy, INT a_ex, INT a_ey, Gdiplus::ARGB a_color);
+	void DrawLine(INT a_sx, INT a_sy, INT a_ex, INT a_ey);
+	void MoveTo(INT a_x, INT a_y);
+	void LineTo(INT a_x, INT a_y);
 
-	void SetDCBrushColor(ARGB a_color);
-	void SetDCPenColor(ARGB a_color);
-	void SetDCPenColor(DashStyle a_style, REAL a_width, ARGB a_color);
-	void SetTextColor(ARGB a_color);
+	// 브러시 색상 변경
+	void SetDCBrushColor(Gdiplus::ARGB a_color);
+	// 펜 색상 변경
+	void SetDCPenColor(Gdiplus::ARGB a_color);
+	// 펜 변경 (스타일, 굵기, 색상)
+	void SetDCPenColor(Gdiplus::DashStyle a_style, Gdiplus::REAL a_width, Gdiplus::ARGB a_color);
+	// 글꼴 색상 변경
+	void SetTextColor(Gdiplus::ARGB a_color);
+	// 폰트 객체 재설정 , 이전 폰트 객체의 주소를 반환함
 	Gdiplus::Font* SelectObject(Gdiplus::Font* ap_font);
 
-	void Draw(Image* ap_image, int a_x, int a_y);
-	void Draw(HDC ah_dc, int a_x = 0, int a_y = 0);
+	// 텍스트 출력 (색상부여)
+	void GpTextOut(INT a_x, INT a_y, Gdiplus::ARGB a_color, const TCHAR* ap_str, INT a_str_len);
+	// 텍스트 출력 (기존색상)
+	void GpTextOut(INT a_x, INT a_y, const TCHAR* ap_str, INT a_str_len);
 
-	void TextOut(int a_x, int a_y, ARGB a_color, const TCHAR* ap_str, int a_str_len);
-	void TextOut(int a_x, int a_y, const TCHAR* ap_str, int a_str_len);
+	// 이미지 객체 출력
+	void Draw(Gdiplus::Image* ap_image, INT a_x, INT a_y);
+	// DC에 현재 이미지 출력
+	void Draw(HDC ah_dc, INT a_x = 0, INT a_y = 0);
 
-	Image* GetImage() { return mp_bitmap; }
-	Gdiplus::Graphics* GetGraphics() { return mp_graphic; }
+	// Get
+	Gdiplus::Image* GetImage();
+	Gdiplus::Graphics* GetGraphics();
 
-	operator Image* () { return mp_bitmap; }
-	operator Gdiplus::Graphics* () { return mp_graphic; }
+	// Get함수들을 편하게 사용할 수 있도록 연산자 오버로딩
+	operator Gdiplus::Image* ();
+	operator Gdiplus::Graphics* ();
+
+	// 전체 영역 색상 채우기
+	void Clear(Gdiplus::ARGB a_color);
+	void Clear();
+
+	// 다각형 그리기 (채우기)
+	void FillPolygon(Gdiplus::Point* ap_position_list, INT a_count);
+
+	// 다각형 그리기 (테두리)
+	void DrawPolygon(Gdiplus::Point* ap_position_list, INT a_count);
+
+	// Arc 그리기 (테두리)
+	void DrawArc(Gdiplus::REAL a_x, Gdiplus::REAL a_y, Gdiplus::REAL a_cx, Gdiplus::REAL a_cy, Gdiplus::REAL a_start_angle, Gdiplus::REAL a_sweep_angle);
+
+	// 이미지 경로를 넘겨 출력
+	void Draw(TCHAR* a_file_path, INT a_x, INT a_y);
 };
 
 #endif
