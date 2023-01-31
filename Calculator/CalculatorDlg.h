c9701e635320ea00e832ff6f2400a108db83c959
tjdwhd70599@gmail.com <tjdwhd70599@gmail.com>
2023-01-31 12:09:19 +0900

    계산기 구상

---
+++ Calculator/CalculatorDlg.h
@@ -0,0 +1,39 @@
+
+// CalculatorDlg.h : header file
+//
+
+#pragma once
+
+
+// CCalculatorDlg dialog
+class CCalculatorDlg : public CDialogEx
+{
+// Construction
+public:
+	CCalculatorDlg(CWnd* pParent = nullptr);	// standard constructor
+
+// Dialog Data
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_CALCULATOR_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
+
+
+// Implementation
+protected:
+	HICON m_hIcon;
+
+	// Generated message map functions
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedOk();
+	afx_msg void OnBnClickedCancel();
+	afx_msg void OnClose();
+	afx_msg void OnDestroy();
+};
